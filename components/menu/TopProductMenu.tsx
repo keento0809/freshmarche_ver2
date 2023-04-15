9fcd27ae609389da346f010081e07955fa924c9f
KEENTO <handball0321@gmail.com>
2023-04-14 21:04:38 -0700

    Revised type definitions

--- components/menu/TopProductMenu.tsx
+++ components/menu/TopProductMenu.tsx
@@ -1,11 +1,11 @@
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import { topProductsMenuContents } from "@/constants/product/topProducts";
+import { topProductsMenu } from "@/constants/product/topProducts";
 
 export default function TopProductMenu() {
   return (
     <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
-      {topProductsMenuContents.contents.map((menu) => {
+      {topProductsMenu.map((menu) => {
         return (
           <Typography variant="body1" sx={{ minWidth: 100 }}>
             {menu}
--- constants/hero/hero.ts
+++ constants/hero/hero.ts
@@ -1,8 +1,8 @@
 interface HeroDescription {
-  description: string;
+  readonly description: string;
 }
 
-export const heroDescription: Readonly<HeroDescription> = {
+export const heroDescription: HeroDescription = {
   description:
     "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
 };
