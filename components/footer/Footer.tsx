e6ed257618479400f30039d029bcb0a1b6f3142a
KEENTO <handball0321@gmail.com>
2023-04-15 15:54:57 -0700

    Created the copyright part in the default Footer component

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import Box from "@mui/material/Box";
+import { copyright } from "@/constants/footer/footer";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Box
+      component="footer"
+      sx={{ padding: "4rem 0", fontSize: "12px", letterSpacing: "0.5px" }}
+    >
+      <Box>{copyright.text}</Box>
+    </Box>
+  );
 }
---
+++ constants/footer/footer.ts
@@ -0,0 +1,7 @@
+interface Copyright {
+  readonly text: string;
+}
+
+export const copyright: Copyright = {
+  text: "© 2023 Kento Honda. All Rights Reserved.",
+};
