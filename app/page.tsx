234da12695c6ea61fa5aeec8920b31972d056ed0
KEENTO <handball0321@gmail.com>
2023-04-14 17:04:52 -0700

    Created the default TopProduct component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero/Hero";
+import TopProducts from "@/components/product/TopProducts";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <TopProducts />
     </main>
   );
 }
---
+++ constants/product/topProducts.ts
@@ -0,0 +1,7 @@
+interface TopProductsMenuContents {
+  contents: [string, string, string, string];
+}
+
+export const topProductsMenuContents: Readonly<TopProductsMenuContents> = {
+  contents: ["Top", "Menu1", "Menu2", "Menu3"],
+};
