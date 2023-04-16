ca44b78df249077c029d7c94d4db502911cf7258
KEENTO <handball0321@gmail.com>
2023-04-15 17:39:54 -0700

    Added the server component for test

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,35 @@
-export default function About() {
-  return <div>About page</div>;
+"use client";
+
+import { getTestData } from "@/app/server/test/test";
+import { Box, Typography } from "@mui/material";
+
+interface TestD {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export default async function About() {
+  const testData: TestD[] = await getTestData();
+
+  return (
+    <Box sx={{ padding: "6rem 0" }}>
+      About page
+      <Box>
+        {testData?.map((d) => {
+          return (
+            <div key={d.id}>
+              <Typography variant="h6" sx={{ padding: "1rem" }}>
+                {d.title}
+              </Typography>
+              <Typography variant="body1" sx={{ padding: "2rem" }}>
+                {d.body}
+              </Typography>
+            </div>
+          );
+        })}
+      </Box>
+    </Box>
+  );
 }
---
+++ app/server/test/test.ts
@@ -0,0 +1,11 @@
+export async function getTestData() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=10`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
