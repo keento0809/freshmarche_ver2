fb691232015f0427ba8f846fdfce34d229309852
KEENTO <handball0321@gmail.com>
2023-10-03 08:51:10 +0900

    Cleaned up ViewWrapper component

--- src/app/(pages)/products/[id]/ProductPage.tsx
+++ src/app/(pages)/products/[id]/ProductPage.tsx
@@ -1,16 +1,8 @@
 "use client";
 
-import { BgWrapper } from "../../../components/wrapper/BgWrapper";
-import { ViewWrapper } from "../../../components/wrapper/ViewWrapper";
 import { useParams } from "next/navigation";
 
 export const ProductPage = () => {
   const { id } = useParams();
-  return (
-    <BgWrapper>
-      <ViewWrapper>
-        <div>Product: {id}</div>
-      </ViewWrapper>
-    </BgWrapper>
-  );
+  return <div>Product: {id}</div>;
 };
