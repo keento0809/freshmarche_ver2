cf95ea4d6fe898274fadda38ad8550a837a53719
KEENTO <handball0321@gmail.com>
2023-11-07 07:19:27 +0900

    Separeted logics and styles from Nav component

---
+++ src/components/nav/useNav.ts
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export const useNav = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+    useState<null | HTMLElement>(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+  return {
+    anchorEl,
+    mobileMoreAnchorEl,
+    isMenuOpen,
+    isMobileMenuOpen,
+    handleProfileMenuOpen,
+    handleMobileMenuClose,
+    handleMobileMenuOpen,
+    handleMenuClose,
+  };
+};
