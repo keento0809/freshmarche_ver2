9cd4156e5da5d6be25a45706680e769762f961e9
KEENTO <handball0321@gmail.com>
2023-11-07 10:32:59 +0900

    Added not-found page

---
+++ src/app/NotFoundPage.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { FC } from "react";
+import Link from "next/link";
+import { Box, Typography } from "@mui/material";
+import { FlexWrapper } from "../components/wrapper/FlexWrapper";
+
+const styles = {
+  position: "relative",
+  height: "100vh",
+  paddingBottom: "2rem",
+};
+
+export const NotFoundPage: FC = () => {
+  return (
+    <FlexWrapper styles={styles}>
+      <Typography variant="body1" sx={{ py: 4 }}>
+        Page Not Found
+      </Typography>
+      <Link href={"/home"}>Go Back</Link>
+    </FlexWrapper>
+  );
+};
