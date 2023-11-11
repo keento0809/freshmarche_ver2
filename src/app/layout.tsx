2a9416b08580674b95f0a5beee6d1d78534843ea
KEENTO <handball0321@gmail.com>
2023-11-11 09:23:51 +0900

    Added the basic next-auth middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "@/src/components/footer/Footer";
 import { ReactQueryProvider } from "../providers/ReactQueryProvider";
 import { BgWrapper } from "../components/wrapper/BgWrapper";
 import SessionProvider from "../providers/SessionProvider";
-import { getServerAuthSession } from "./api/auth/[...nextauth]";
+import { getServerAuthSession } from "./api/auth/[...nextauth]/route";
 
 export const metadata = {
   title: "TechMarche",
