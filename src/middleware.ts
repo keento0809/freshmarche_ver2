c2f8cbdd5780e608868e6e410919fa98c37afe62
KEENTO <handball0321@gmail.com>
2023-11-13 09:05:36 +0900

    Deleted unnecessary contents

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,23 +3,3 @@ export { default } from "next-auth/middleware";
 export const config = {
   matcher: ["/protected", "/user/:path*"],
 };
-
-// import { NextRequest, NextResponse } from "next/server";
-
-// export default async function middleware(req: NextRequest) {
-//   const path = req.nextUrl.pathname;
-//   const session = !!req.cookies.get("next-auth.session-token");
-
-//   if (!session) {
-//     return NextResponse.redirect(new URL(`http://localhost:3000/login`));
-//   }
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: [
-//     "/((?!api|_next/static|_next/image|favicon.ico).*)",
-//     "/protected",
-//     "/workspace/:path*",
-//   ],
-// };
