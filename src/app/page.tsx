73e5bb28058932e25a11f7a7dbf76a6154447044
KEENTO <handball0321@gmail.com>
2024-01-24 08:31:02 +0900

    Add: shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-import { LandingPage } from "./LandingPage";
+"use client";
+
+import Link from "next/link";
+import { Box, Typography } from "@mui/material";
+import { FlexWrapper } from "@/src/components/wrapper/FlexWrapper";
+import { landingPageTexts } from "@/src/constants/landingPage/landingPage";
+
+const styles = {
+  height: "100vh",
+  gap: "2.5rem",
+  zIndex: "10",
+};
 
 export default function Home() {
   return (
     <main>
-      <LandingPage />
+      <FlexWrapper styles={styles}>
+        <Box sx={{ zIndex: "10" }}>
+          <Typography
+            variant="h2"
+            sx={{ letterSpacing: "-1px", fontSize: "32px", fontWeight: "400" }}
+          >
+            {landingPageTexts.title}
+          </Typography>
+          <Link href={"/home"}>Get started</Link>
+        </Box>
+      </FlexWrapper>
     </main>
   );
 }
