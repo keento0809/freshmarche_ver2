05c29d668f29a8a44fba34012bb6c41e0116477f
KEENTO <handball0321@gmail.com>
2024-02-06 07:24:40 +0900

    Fix: type error

--- src/app/(auth)/login/LoginPage.tsx
+++ src/app/(auth)/login/LoginPage.tsx
@@ -8,11 +8,9 @@ import { LoginForm } from "@/src/components/form/LoginForm";
 
 export const LoginPage: FC = () => {
   return (
-    <Box>
-      <FlexWrapper>
-        <MainTitle title={"Login"} />
-        <LoginForm />
-      </FlexWrapper>
-    </Box>
+    <FlexWrapper>
+      <MainTitle title={"Login"} />
+      <LoginForm />
+    </FlexWrapper>
   );
 };
