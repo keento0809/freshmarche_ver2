1fc7a7c1976621bd12d755540f4803bab233c5e1
KEENTO <handball0321@gmail.com>
2024-04-07 13:16:53 +0900

    Fix: type error on server.ts

--- src/app/(auth)/signup/SignupPage.tsx
+++ src/app/(auth)/signup/SignupPage.tsx
@@ -6,7 +6,11 @@ import { useSignupPage } from "./_hooks/useSignupPage";
 import { redirect } from "next/navigation";
 import { SignupForm } from "@/src/components/form/SignupForm";
 
-export const SignupPage: FC = () => {
+type SignupPageProps = {
+  searchParams: { message: string };
+};
+
+export const SignupPage: FC<SignupPageProps> = ({ searchParams }) => {
   const { isSuccess, onSubmit, form } = useSignupPage();
 
   if (isSuccess) redirect("/home");
@@ -15,6 +19,13 @@ export const SignupPage: FC = () => {
     <div className="min-h-[calc(100svh_-_80px)] flex flex-col justify-center items-center gap-8 pb-10 lg:pb-20">
       <MainTitle title={"Sign up"} />
       <SignupForm onSubmit={onSubmit} form={form} />
+      <div className="py-4">
+        {searchParams?.message && (
+          <p className="bg-foreground/10 text-foreground text-center">
+            {searchParams.message}
+          </p>
+        )}
+      </div>
     </div>
   );
 };
--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,15 +1,18 @@
-import { supabase } from "@/src/supabase";
+import { createClient } from "@/src/utils/supabase/server";
 import { SignupUser } from "@/src/types/auth";
 import { NextRequest, NextResponse } from "next/server";
+import { redirect } from "next/navigation";
 
 export async function POST(req: NextRequest) {
   if (req.method === "POST") {
+    const supabase = createClient();
     try {
       const body = (await req.json()) as SignupUser;
-      const { data } = await supabase.auth.signUp({
+      const { data, error } = await supabase.auth.signUp({
         email: body.email,
         password: body.password,
       });
+      if (error) redirect("/signup?messages=Could not signup");
       return NextResponse.json(data, { status: 200 });
     } catch (err) {
       if (err instanceof Error) console.log(err.message);
